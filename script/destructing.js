caf804d1f3bd2fe6c1f04324f8e7ceef9cf34314
hemantjatav <hemanthikke@gmail.com>
2022-11-08 17:33:33 +0530

    6.5

--- script/destructing.js
+++ script/destructing.js
@@ -4,7 +4,7 @@
 //let [fname,age=0,cgpa]=studentDetails wrong 
 let [fname,age,cgpa,branch,collge="XYZ",company="CTS"]=studentDetails
 console.log(fname)
-console.log(age)
+console.log(age)`
 console.log(cgpa)
 console.log(branch)
 let fruits="Apple Orange Pineapple"
@@ -19,7 +19,7 @@ console.log(al1)
 console.log(al2)
 console.log(al3)
 console.log(al4)
-console.log(al5)*/
+console.log(al5)
 
 //destrcuting an object
 let studentDetails={
@@ -41,4 +41,99 @@ let {CTS:a,Wipro:c,TCS:b,Capgemini:d="50,000"}=company
 console.log(a)
 console.log(b)
 console.log(c)
-console.log(d)
+console.log(d)*/
+
+//practice
+let studentDetails=["peter",25,7.8 ,"ECE"]
+let [fname,age,cgpa,branch,collge="DU"]=studentDetails
+console.log(fname)
+console.log(age)
+console.log(cgpa)
+console.log(branch)
+console.log(collge)
+
+let fruits ="Apple Orange Pineapple"
+let [fr1,fr2,fr3]=fruits.split(" ")
+console.log(fruits.split(" "))
+console.log(fr1)
+console.log(fr2)
+console.log(fr3)
+
+
+
+let company={
+    wipro:"40000",
+    samsung:"50000",
+    hire:"34200"
+    
+}
+let {wipro:w,...extra}=company
+console.log(typeof extra)
+//console.log(typeof extra)
+let canidate={salary:"500000",...extra,company:"cts"}
+console.log(canidate)
+
+
+function showDetails(...details){
+    console.log(details)
+    let [fname,age,city]=details
+   // console.log(`Studentname:${fname} city:${city} age:${age}`)
+}
+showDetails("peter",29,"mumbai","colloge","40000","50000","34200"
+)
+
+
+let add=(a,b)=>a+b
+let result=add(34,3)
+console.log(result)
+
+
+let areaOfsquare=(side)=>side*side
+let take=areaOfsquare(8)
+console.log(take)
+
+const ano=(side)=>{
+    console.log("this a function")
+    console.log("second line")
+    return side*side
+}
+let result2=ano(8)
+console.log(result2)
+
+
+let a=10
+console.log(++a)
+console.log(a++)
+console.log(a)
+console.log((a++)+10)
+
+let option ="coffee"
+if (option==="coffee"){
+    console.log("i want coffee")
+}
+else{
+    console.log("i want tea")
+}
+option==="coffee"?console.log("i want coffee"):console.log("i want tea")
+
+
+
+
+  
+(function(a){
+    return (function(){
+      console.log(a);
+      a = 6;
+    })()
+   })(21);
+   
+   
+   
+   var aa = 1;  
+   var bb = 0;  
+   while (aa <= 3)  
+   {  
+      aa++;  
+      bb += aa * 2;  
+      print(bb);
+   }
